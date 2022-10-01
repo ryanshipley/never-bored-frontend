a3dc7a86ea15081f487c80c43f492290ef41ba4c
Ryan Shipley <ryanshipley23@gmail.com>
2022-10-01 08:44:00 -0400

    adds to profile page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Nav from "./components/Nav";
+import NAV from "./components/Nav";
+
 function Profile(props){
     return (
     <div className="Profile">
         <NAV/>
         <h1>Profile Page</h1>;
-
+        <img src={user.image} />
+        <h3>About Us: {user.about}</h3>
     </div>
-    )
+    );
 };
 
 export default Profile;
