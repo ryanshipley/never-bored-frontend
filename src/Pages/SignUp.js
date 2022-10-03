393c8acf0bc0f25aac960b7512390f1aa1c2599e
Ryan Shipley <ryanshipley23@gmail.com>
2022-10-03 09:12:22 -0400

    adds handleSubmit

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function SignUp(props){
-    return <h1>Sign up</h1>;
+    const [ newForm, setNewForm ] = React.useState({
+        name: "",
+        email: "",
+        image: "",
+    });
+    const handleChange = e =>{
+        setNewForm({ ...newForm, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        props.createUser(newForm);
+        setNewForm({
+            name:"",
+            email: "",
+            image: "",
+        });
+    };
+    
 };
 
 export default SignUp;
