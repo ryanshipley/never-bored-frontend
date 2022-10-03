a922e71d9a50d44b1ee1e7226f17761cbfc73266
Ryan Shipley <ryanshipley23@gmail.com>
2022-10-03 10:12:24 -0400

    fix profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -5,7 +5,7 @@ function Profile(props){
     return (
     <div className="Profile">
         <NAV/>
-        <h1>Profile Page</h1>;
+        <h1>Profile Page</h1>
         <img src={user.image} />
         <h3>About Us: {user.about}</h3>
     </div>
