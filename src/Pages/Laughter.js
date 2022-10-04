a671af324d88b297cd5310a31c37791446e7bf52
Mary Rossi <maryrossi106@gmail.com>
2022-10-03 23:40:13 -0500

    added text to pages

--- src/Pages/Laughter.js
+++ src/Pages/Laughter.js
@@ -4,6 +4,7 @@ function Laughter() {
     return (
         <div>
             <h1>Fun Activities</h1>
+            <p>Welcome to the Fun Activities Page! Here you can click the button below and have a new activity shown for you and your partner to enjoy!</p>
             <button>Find new Activity</button>
             <img src="fun-couple.jpeg" alt="Fun activity"/>
         </div>
--- src/Pages/Love.js
+++ src/Pages/Love.js
@@ -4,6 +4,7 @@ function Love() {
   return (
     <div>
       <h1>Romantic Activities</h1>
+      <p>Welcome to the Romantic Activities Page! Here you can click the button below and have a new activity shown for you and your partner to enjoy!</p>
       <button>Find New Activity</button>
       <img src="romantic-couple.jpeg" alt="Romantic Activity"/>
     </div>
