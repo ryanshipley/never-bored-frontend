ad60aa5c33bbc114eb56987765a39d2d7674d9e3
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-06 22:31:38 -0400

    center name activity elements

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">
-            <img src="https://imgur.com/UmxUBos.png" alt="app-logo" />
-        </Link>
+      <Link to="/">
+        <img src="https://imgur.com/UmxUBos.png" alt="app-logo" />
+      </Link>
+      <Link to="/" className='align-center'>
         <h2>TheNeverBoredCouple</h2>
+      </Link>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
