7b19b7a7601cfa4884a7fa8ee75b3ca6b5d177e1
Mary Rossi <maryrossi106@gmail.com>
2022-10-06 19:44:10 -0500

    updated pages

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import { useState, } from 'react'
+import { useState, useEffect } from 'react'
 
 function Show (props) {
-
+  // let activities = [];
+    // useEffect(() => {
+    //   console.log("props:", props)
+    //   if (props.activities) {
+    //     activities = props.activities;
+    //     localStorage.setItem('activities', activities);
+    //   } else {
+    //     activities = localStorage.getItem('activities');
+    //   }
+    // });
     const id = props.match.params.id;
-    const activities = props.activities;
+    // activities = props.activities;
+    const activities = [
+      {
+        _id: id,
+        activityName: "Picnic",
+        type: "Cool",
+        accesibility: 4,
+        description: "eat some food",
+      }
+    ];
+
     // _id is coming from our backend/database MongoDb
     const activity = activities.find(p => p._id === id);
     console.log(activity)
@@ -27,12 +46,14 @@ function Show (props) {
     // }
 
   return (
-    <div className="activity">
+    <div id='show-page'>
+        <div className="show-data">
         <h1>{activity.activityName}</h1>
         <h2>{activity.type}</h2>
         <h2>{activity.accesibility}</h2>
         <h2>{activity.description}</h2>
         <img src={activity.image} alt={activity.name} />
+        </div>
         {/* <form onSubmit={handleSubmit}>
           <input
               type="text"
